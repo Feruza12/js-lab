5751f55c910b98cfac870a8972d71fa198ab7ddb
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-09-26 22:54:03 +0500

    created folders regarding modular architecture

---
+++ to_do_app/homework/src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello world</p>
+    </>
+  )
+}
+
+export default App
