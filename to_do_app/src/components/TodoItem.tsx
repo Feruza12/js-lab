1df0bfa87422da323ee5e10f9bf5a03405c63895
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-09-27 00:55:44 +0500

    #

--- to_do_app/src/components/TodoItem.tsx
+++ to_do_app/src/components/TodoItem.tsx
@@ -10,15 +10,21 @@ interface TodoItemProps {
 const TodoItem: React.FC<TodoItemProps> = ({ todo, onToggle, onRemove }) => {
   return (
     <div className="flex w-1/2 border rounded justify-between items-center">
-      <div onClick={() => onToggle(todo.id)} className="flex items-center justify-between p-2 cursor-pointer w-full">
+      <div
+        onClick={() => onToggle(todo.id)}
+        className="flex items-center justify-between p-2 cursor-pointer w-full text-center"
+      >
         <p
           onClick={() => onToggle(todo.id)}
           className={
-            todo.completed ? "line-through text-gray-500 cursor-pointer" : "cursor-pointer" 
+            todo.completed
+              ? "line-through text-gray-500 cursor-pointer"
+              : "cursor-pointer"
           }
         >
           {todo.task}
         </p>
+        <p>{todo.completed ? "Completed!" : ""}</p>
       </div>
 
       <div>
